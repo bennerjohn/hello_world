158336c37f5fb64451e191552c07865bfd594216
bennerjohn <bennerj@ELABSB138-11.easternct.edu>
2015-09-09 14:48:02 -0400

    Assignment Changes
    Made it ask for a name.

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-  
-  cout << "Hello Class!\n\n";
 
+int main () {
+  string name;
+  cout << "What is your name?" << endl;
+  cin  >> name;
+  cout << "Hello " << name << " my name is John." << endl;;
   return 0;
 }
 
